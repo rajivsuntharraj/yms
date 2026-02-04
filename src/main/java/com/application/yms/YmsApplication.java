91ea5c6678855fee2ea2e96eaedea521d10b3edc
rajivsuntharraj <rajivmtech15@gmail.com>
2026-02-04 12:04:05 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/application/yms/YmsApplication.java
@@ -0,0 +1,13 @@
+package com.application.yms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/yms/YmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.yms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
